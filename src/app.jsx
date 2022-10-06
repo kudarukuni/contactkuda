328f3a9262f8927462e182afed438395a987dfd6
Kuda Rukuni <52784179+kudarukuni@users.noreply.github.com>
2022-10-06 12:04:54 +0200

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -5,39 +5,28 @@ export default function App() {
   return (
     <div className="mainContainer">
       <div className="dataContainer">
+    
         <div className="header">
-            <p class="red-text-shadow"><b>👋 ABOUT KUDA !! 👋</b></p>
+            <p class="red-text-shadow"><strong>👋 ABOUT KUDA !! 👋</strong></p>
         </div>
+        
         <div className="totalWaves">
-          <details>
-            <summary>
-              <b>Description</b>
-            </summary>
-          </details>
+          <b>Description</b>
         </div>
+    
         <div className="bio">
           <font color="white">
           I am KUDA RUKUNI and I have worked on a number of IoT projects for some of the biggest and top IoT companies in Africa.<br/><br/>+263782516263<br/><br/>krukuni@gmail.com
           </font>
         </div>
-        <button class="custom-btn waveButton">
-          <span>
-            <i class="fa fa-phone" aria-hidden="true"></i>
-            <b>
-              <a href="tel:+263782516263" target="_blank">
-                <font color = "black">CALL KUDA NOW!!</font>
-              </a>
-            </b>
-          </span>
-        </button>
-        <button class="custom-btn waveButton">
-          <i class="fa fa-envelope" aria-hidden="true"></i>
-          <b>
-            <a href="mailto:krukuni@gmail.com" target="_blank">
-              <font color = "black">SEND EMAIL!!</font>
-            </a>
-          </b>
-        </button>
+    
+        <span>
+          <a href="tel:+263782516263" target="_blank" class="button"><i class="fa fa-phone" aria-hidden="true">Call Kuda Now !!</i></a>
+          <a href="mailto:krukuni@gmail.com" target="_blank" class="button"><i class="fa fa-envelope" aria-hidden="true">Send Mail Now !!</i></a>
+          <a href="kudarukuni-kudarukuni.vercel.app" target="_blank" class="button"><i class="fa fa-desktop" aria-hidden="true">Desktop Site !!</i></a>
+          <a href="cvtemp-kudarukuni.vercel.app" target="_blank" class="button"><i class="fa fa-mobile" aria-hidden="true">Mobile Site !!</i></a>
+        </span>
+    
       </div>
     </div>
   );
