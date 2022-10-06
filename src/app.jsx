25d52d0a7227dace129deae33af6122151090f45
Kuda Rukuni <52784179+kudarukuni@users.noreply.github.com>
2022-10-06 15:23:53 +0200

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -6,17 +6,17 @@ export default function App() {
     <div className="mainContainer">
       <div className="dataContainer">    
         <div className="header">
-            <p class="red-text-shadow"><strong>👋 ABOUT KUDA !! 👋</strong></p>
+            <p class="red-text-shadow"><strong>ABOUT KUDA</strong></p>
         </div>        
         <div className="totalWaves">
-          <b>Description</b>
+          <b><font color="gold">Description</font></b>
         </div>    
         <div className="bio">
-          <font color="white">I am KUDA RUKUNI and I have worked on a number of IoT projects for some of the biggest and top IoT companies in Africa.
-          <br/><br/>
-          <a href="tel:+263782516263" target="_blank"><i class="fa fa-phone" aria-hidden="true"><strong>+263782516263</strong></i></a>
-          <br/><br/>
-          <a href="mailto:krukuni@gmail.com" target="_blank" class="button"><i class="fa fa-envelope" aria-hidden="true"><strong>krukuni@gmail.com</strong></i></a>
+          <font color="white">Hi, 👋 I am Kuda Rukuni and I am a Full Stack IoT, Web3.0 and Software Engineer. 
+            I have worked on a number of IoT projects for some of the biggest and top IoT companies in Africa.</font>
+            <br/>For more information and booking please call me on this number <a href="tel:+263782516263" target="_blank"><strong>+263 782 51 6263.</strong></a>
+            <br/>Otherwise if you prefer to communicate via email, please feel free to send mail to this address <a href="mailto:krukuni@gmail.com" target="_blank"><strong>krukuni@gmail.com.</strong></a>
+            <br/>I am readily available for all discussions and please do not hesitate to hit the WhatsApp button on the bottom right of your screen to start a conversation.
           </font>          
           <p align="center"><a href="kudarukuni-kudarukuni.vercel.app" target="_blank" class="button"><i class="fa fa-desktop" aria-hidden="true">Desktop Site !!</i></a></p>
           <p align="center"><a href="cvtemp-kudarukuni.vercel.app" target="_blank" class="button"><i class="fa fa-mobile" aria-hidden="true">Mobile Site !!</i></a></p>
