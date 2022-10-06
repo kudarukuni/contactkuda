f4674cd478b9e9986492dba3b90f4b3d7f7b4548
Kuda Rukuni <52784179+kudarukuni@users.noreply.github.com>
2022-10-06 12:20:13 +0200

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -19,12 +19,9 @@ export default function App() {
           <br/><br/>+263782516263<br/><br/>krukuni@gmail.com</font>
           <p align="center"><a href="tel:+263782516263" target="_blank" class="button"><i class="fa fa-phone" aria-hidden="true">Call Kuda Now !!</i></a></p>
           <p align="center"><a href="mailto:krukuni@gmail.com" target="_blank" class="button"><i class="fa fa-envelope" aria-hidden="true">Send Mail Now !!</i></a></p>
+          <p align="center"><a href="kudarukuni-kudarukuni.vercel.app" target="_blank" class="button"><i class="fa fa-desktop" aria-hidden="true">Desktop Site !!</i></a></p>
+          <p align="center"><a href="cvtemp-kudarukuni.vercel.app" target="_blank" class="button"><i class="fa fa-mobile" aria-hidden="true">Mobile Site !!</i></a></p>
         </div>
-    
-        <p align="center">          
-          <a href="kudarukuni-kudarukuni.vercel.app" target="_blank" class="button"><i class="fa fa-desktop" aria-hidden="true">Desktop Site !!</i></a>
-          <a href="cvtemp-kudarukuni.vercel.app" target="_blank" class="button"><i class="fa fa-mobile" aria-hidden="true">Mobile Site !!</i></a>
-        </p>
     
       </div>
     </div>
