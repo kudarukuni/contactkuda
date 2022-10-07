48ae3d3d854115ee5c56c5d321ed6fd03e50fb6a
Kuda Rukuni <52784179+kudarukuni@users.noreply.github.com>
2022-10-07 08:46:30 +0200

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -6,22 +6,22 @@ export default function App() {
     <div className="mainContainer">
       <div className="dataContainer">    
         <div className="header">
-            <p class="red-text-shadow"><strong>ABOUT KUDA</strong></p>
+            <p class="red-text-shadow"><strong><b><font color="#12BDF3" size="8"><u>ABOUT KUDA</u></font></b></strong></p>
         </div>        
         
-        <div className="totalWaves">
-          <p align="center"><b><font color="gold">Description</font></b></p>
+        <div className="bio">
+          <p align="center"><b><font color="silver">Description</font></b></p>
         </div>   
     
-        <div className="bio">
+        <div className="totalWaves">
           <font color="white">Hi, 👋 I am Kuda Rukuni and I am a Full Stack IoT, Web3.0 and Software Engineer. 
             I have worked on a number of IoT projects for some of the biggest and top IoT companies in Africa.
             <br/><br/><br/>
-            For more information and booking please call me on this number</font><a href="tel:+263782516263" target="_blank">            
+            For more information and booking please call me on this number </font><a href="tel:+263782516263" target="_blank">            
             <font color="gold">+263 782 51 6263</font></a><font color="white"> and if you prefer to communicate via email, please feel free to send mail to this address </font> 
             <a href="mailto:krukuni@gmail.com" target="_blank"><font color="gold">krukuni@gmail.com</font></a><font color="white">.
             <br/><br/><br/>
-            I am readily and always available for all discussions. And please do not hesitate to hit the</font><font color="#075e54"> WhatsApp</font><font color="white"> button 
+            I am readily and always available for all discussions. And please do not hesitate to hit the</font><font color="#075e54"><a href="https://api.whatsapp.com/send/?phone=263782516263&text&type=phone_number&app_absent=0" target="_blank"> WhatsApp</a></font><font color="white"> button 
             on the bottom right of your screen to start a conversation on my favourite Messenger Application.
           </font>          
           <p align="center">
